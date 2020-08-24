4b87e726abd6583e2892a28955f2cc5a742cf350
vanbierhoff <vanbierhoff@mail.ru>
2020-08-24 15:02:36 +0300

    ready

--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,8 +1,7 @@
-import { ID } from '@datorama/akita';
-import { Notes } from './store-notes/notes.model';
+
 import { Observable, Subscription } from 'rxjs';
 import { Component, OnInit, Input } from '@angular/core';
-import {NotesService } from './store-notes/notes.service';
+import { NotesService } from './store-notes/notes.service';
 import { NotesQuery } from './store-notes/notes.query';
 
 
@@ -14,35 +13,35 @@ import { NotesQuery } from './store-notes/notes.query';
 export class HeadComponent implements OnInit {
 
   constructor(
-    private  NotesQuery: NotesQuery, private NotesService: NotesService
+    private NotesQuery: NotesQuery, private NotesService: NotesService
   ) {
   }
   notes: [];
   combine$: Subscription;
   ngOnInit(): void {
-    this.combine$ = this.NotesQuery.combine$.subscribe(notes => { 
+    this.combine$ = this.NotesQuery.combine$.subscribe(notes => {
       this.notes = notes
     });
   }
 
-  addNotes(name: string ): void{
+  addNotes(name: string): void {
     this.NotesService.addNotes(name);
   }
   setFilter(filter: string): void {
-   this.NotesService.setFilter(filter);
+    this.NotesService.setFilter(filter);
   }
 
-  update( notes: object): void {
-    let  id = notes['id'];
+  update(notes: object): void {
+    let id = notes['id'];
     this.NotesService.update(id, notes['done'])
   }
 
-  remove(id:string): void {
+  remove(id: string): void {
     this.NotesService.removeEl(id);
   }
   editName(options: object): void {
     this.NotesService.updateName(options['id'],
-    options['value']);
+      options['value']);
   }
 
 }
--- src/app/head/store-notes/notes.service.ts
+++ src/app/head/store-notes/notes.service.ts
@@ -1,27 +1,27 @@
-import {Injectable} from '@angular/core';
-import {NotesStore} from './notes.store';
-import {Notes, createNotes} from './notes.model';
+import { Injectable } from '@angular/core';
+import { NotesStore } from './notes.store';
+import { Notes, createNotes } from './notes.model';
 
 // Интеграция стора с angular, уточнить как работает?
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class NotesService {
 
   constructor(
     private notesStore: NotesStore
   ) {
   }
-        // добавляем заметку в стор
+  // добавляем заметку в стор
   addNotes(data: string): void {
     const notes = createNotes(data);
     this.notesStore.add(notes);
   }
 
-  setFilter(filter: string ): void {
-    this.notesStore.update({filter});
+  setFilter(filter: string): void {
+    this.notesStore.update({ filter });
   }
 
-  update(id:string, done: boolean): void {
-    this.notesStore.update(id, {done})
+  update(id: string, done: boolean): void {
+    this.notesStore.update(id, { done })
   }
 
 
@@ -30,7 +30,7 @@ export class NotesService {
     this.notesStore.remove(id);
   }
 
-  updateName(id:string,name:string){
-    this.notesStore.update(id, {name})
+  updateName(id: string, name: string) {
+    this.notesStore.update(id, { name })
   }
 }
